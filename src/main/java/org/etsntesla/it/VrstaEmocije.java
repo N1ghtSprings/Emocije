b2fd8cba10149677a7087cbe7ffa2b8f72b32e9e
Filip Pesic <10219filippesic@etsntesla.edu.rs>
2023-02-27 15:50:02 +0100

    10. video zavrsen

---
+++ src/main/java/org/etsntesla/it/VrstaEmocije.java
@@ -0,0 +1,14 @@
+package org.etsntesla.it;
+
+public enum VrstaEmocije {
+    Radost,
+    Ljutnja,
+    Tuga,
+    Strah,
+    Ljubav,
+    nada,
+    Zavist,
+    Ljubomora,
+    Ponos,
+    Smirenost,
+}
